297dfdcf0bfe04702492a2486beb75fcbbeac79b
Shaun Springer <shaun.springer@gmail.com>
2012-06-28 22:30:34 -0400

    added polyfill requestAnimationFrame

--- bootstrap-plus-draw.js
+++ bootstrap-plus-draw.js
@@ -16,7 +16,7 @@
       this.date = new Date()
       this.$element = $(element).parent()
         .on('mousedown.draw.data-api', '[data-draw="canvas"]', $.proxy(this.start, this))
-        .on('click', '[data-clear="draw"]', $.proxy(this.clear, this))
+        .on('click.draw.data-api', '[data-clear="draw"]', $.proxy(this.clear, this))
       this.$canvas = $(canvas, this.$element)
       this.c = this.$canvas.get(0).getContext('2d')
       this.offset = this.$element.offset()
@@ -53,6 +53,7 @@
 
     , move: function(e) {
       e && e.preventDefault()
+      window.requestAnimationFrame(this.move)
 
       var x = e.offsetX
         , y = e.offsetY
@@ -71,8 +72,10 @@
       var c = this.c
         , $canvas = this.$canvas
       c.clearRect(0, 0, $canvas.width(), $canvas.height());
+      this.points = []
     }
   } 
+
 
  /* DRAW PLUGIN DEFINITION
   * ======================== */
@@ -99,3 +102,35 @@
   })
 
 }(window.jQuery);
+
+
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+ 
+// requestAnimationFrame polyfill by Erik Möller
+// fixes from Paul Irish and Tino Zijdel
+ 
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
